529486c23cac1f42c590455f17c9002f1672566f
Verity Tsai <veritymissed@gmail.com>
2020-03-19 18:33:17 +0800

    Updates

---
+++ precache-manifest.29c0154a9ef6d7ac0b87d7c912056d84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9498a2537be83ffd4cf786fd5660d3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2090bc5672ebd6e6b3b5",
+    "url": "/static/css/2.d9ee7e2f.chunk.css"
+  },
+  {
+    "revision": "2090bc5672ebd6e6b3b5",
+    "url": "/static/js/2.a7824d63.chunk.js"
+  },
+  {
+    "revision": "53f912a32eee53c247f3",
+    "url": "/static/js/main.8a908feb.chunk.js"
+  },
+  {
+    "revision": "e8b9b2add6bf48c020a3",
+    "url": "/static/js/runtime-main.9c4c120a.js"
+  }
+]);
